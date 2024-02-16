4229b54c1463389f5d26a0265cb0953a30a91180
jeff <jeffshen.37@gmail.com>
2024-02-16 14:43:38 -0500

    reddit api setup

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello world')
+import praw
+import json
+
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+reddit = praw.Reddit(
+    client_id = config['client_id'],
+    client_secret = config['client_secret'],
+    user_agent = config['user_agent'],
+    username = config['username'],
+    password = config['password']
+)
+
+for submission in reddit.subreddit('wallstreetbets').hot(limit=5):
+    print(submission.title)
